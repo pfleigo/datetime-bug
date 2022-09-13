8e3634aa5b4f457761c7dec186b5be480b38b3f2
Alexander Pflüger <alexander.pflueger@hybridheroes.de>
2022-09-13 12:27:16 +0200

    print time

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,10 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  time;
 
+  hourMinute = new Intl.DateTimeFormat('de-DE', {
+    hour: '2-digit'
+  });
   constructor() {}
 
+  format(date: string) {
+    if (date) {
+      const options = this.hourMinute.resolvedOptions();
+      console.log(options);
+      return this.hourMinute.format(new Date(date));
+    }
+  }
 }
